53ae17536c900b4613ae8f71ff0ebcba5ca31f6a
MBugajski <michalbugajski@outlook.com>
2021-02-19 23:52:54 +0100

    Deleted the Many entity without affecting the One

---
+++ src/com/mbugajski/springdemo/hib/DeleteCourseDemo.java
@@ -0,0 +1,44 @@
+package com.mbugajski.springdemo.hib;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mbugajski.springdemo.hib.entity.Course;
+import com.mbugajski.springdemo.hib.entity.Instructor;
+import com.mbugajski.springdemo.hib.entity.InstructorDetail;
+import com.mbugajski.springdemo.hib.entity.Student;
+
+public class DeleteCourseDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			session.beginTransaction();
+		
+			Course tempCourse = session.get(Course.class, 10);
+			
+			System.out.println("Deleting course: " + tempCourse);
+			if (tempCourse != null) {
+				session.delete(tempCourse);
+			}
+			
+			session.getTransaction().commit();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
