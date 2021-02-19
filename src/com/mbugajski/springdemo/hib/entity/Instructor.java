aa2adce6fa6e640b680708f2cc82d7f1875298e4
MBugajski <michalbugajski@outlook.com>
2021-02-19 23:13:53 +0100

    Configured the other side of the relationship by adjusting the Instructor class

--- src/com/mbugajski/springdemo/hib/entity/Instructor.java
+++ src/com/mbugajski/springdemo/hib/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.mbugajski.springdemo.hib.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,30 +10,35 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "instructor")
 public class Instructor {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
-	
-	@Column(name="first_name")
+
+	@Column(name = "first_name")
 	private String firstName;
-	
-	@Column(name="last_name")
+
+	@Column(name = "last_name")
 	private String lastName;
-	
-	@Column(name="email")
+
+	@Column(name = "email")
 	private String email;
-	
-	@OneToOne(cascade= {CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
-	@JoinColumn(name="instructor_detail_id")
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
+
+	@OneToMany(mappedBy = "instructor", cascade = { CascadeType.DETACH, CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.REFRESH })
+	private List<Course> courses;
 
 	public Instructor() {
 	}
@@ -81,12 +89,27 @@ public class Instructor {
 		this.instructorDetail = instructorDetail;
 	}
 
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	public void add(Course tempCourse) {
+		if (courses == null) {
+			courses = new ArrayList<Course>();
+		}
+		courses.add(tempCourse);
+
+		tempCourse.setInstructor(this);
+	}
+
 	@Override
 	public String toString() {
 		return "Instructor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", instructorDetail=" + instructorDetail + "]";
 	}
-	
-	
 
 }
