510cf593d564c02a9d8f8a34f5ae7d08a4f9258c
MBugajski <michalbugajski@outlook.com>
2021-02-19 23:35:03 +0100

    Adjusted main apps to create proper entities

--- src/com/mbugajski/springdemo/hib/CreateCoursesDemo.java
+++ src/com/mbugajski/springdemo/hib/CreateCoursesDemo.java
@@ -4,33 +4,38 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.mbugajski.springdemo.hib.entity.Course;
 import com.mbugajski.springdemo.hib.entity.Instructor;
 import com.mbugajski.springdemo.hib.entity.InstructorDetail;
 import com.mbugajski.springdemo.hib.entity.Student;
 
-public class CreateDemo {
+public class CreateCoursesDemo {
 
 	public static void main(String[] args) {
 
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
 				.addAnnotatedClass(Instructor.class)
 				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
 				.buildSessionFactory();
 
 		Session session = factory.getCurrentSession();
 
 		try {
-			Instructor tempInstructor = new Instructor("Adam", "Smith", "adamsmith@gmail.com");
-
-			InstructorDetail tempInstructorDetail = new InstructorDetail("https://www.youtube.com/c/SmithClasses",
-					"smithing");
-
-			tempInstructor.setInstructorDetail(tempInstructorDetail);
-
 			session.beginTransaction();
-			session.save(tempInstructor);
-//			No need to save tempInstructorDetail due to CascadeType.ALL setting in Instructor class
-//			session.save(tempInstructorDetail);
+		
+			int theId = 1;
+			Instructor tempInstructor = session.get(Instructor.class, theId);
+			
+			Course tempCourse1 = new Course("Potato Farming - Latvian Method");
+			Course tempCourse2 = new Course("High Rate Rakes - Tool Smithing Made Easy");
+			
+			tempInstructor.add(tempCourse1);
+			tempInstructor.add(tempCourse2);
+			
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+			
 			session.getTransaction().commit();
 		} catch (Exception exc) {
 			exc.printStackTrace();
---
+++ src/com/mbugajski/springdemo/hib/CreateInstructorDemo.java
@@ -0,0 +1,45 @@
+package com.mbugajski.springdemo.hib;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mbugajski.springdemo.hib.entity.Course;
+import com.mbugajski.springdemo.hib.entity.Instructor;
+import com.mbugajski.springdemo.hib.entity.InstructorDetail;
+import com.mbugajski.springdemo.hib.entity.Student;
+
+public class CreateInstructorDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Course.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			Instructor tempInstructor = new Instructor("Mike", "Smith", "Mikesmith@gmail.com");
+
+			InstructorDetail tempInstructorDetail = new InstructorDetail("https://www.youtube.com/c/SmithClasses",
+					"smithing");
+
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+			session.beginTransaction();
+			session.save(tempInstructor);
+//			No need to save tempInstructorDetail due to CascadeType.ALL setting in Instructor class
+//			session.save(tempInstructorDetail);
+			session.getTransaction().commit();
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
